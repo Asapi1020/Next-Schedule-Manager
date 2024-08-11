f075b70ebeecd81cd1a9f3fdc15aaf1389b931c0
あさぴっぴ1020 <88693257+Asapi1020@users.noreply.github.com>
2024-08-11 15:11:46 +0900

    setup connection with executable GAS API

---
+++ app/lib/gasConfig.ts
@@ -0,0 +1,8 @@
+const config = {
+	clientId: process.env.GAS_CLIENT_ID,
+	clientSecret: process.env.GAS_CLIENT_SECRET,
+	refreshToken: process.env.GAS_REFRESH_TOKEN,
+	scriptId: process.env.GAS_SCRIPT_ID,
+};
+
+export default config;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	env: {
+		GAS_CLIENT_ID: process.env.GAS_CLIENT_ID,
+		GAS_CLIENT_SECRET: process.env.GAS_CLIENT_SECRET,
+		GAS_REFRESH_TOKEN: process.env.GAS_REFRESH_TOKEN,
+		GAS_SCRIPT_ID: process.env.GAS_SCRIPT_ID,
+	},
+};
 
 export default nextConfig;
